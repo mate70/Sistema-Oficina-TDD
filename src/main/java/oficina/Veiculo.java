886c0ee17b7b501c8d9642e142b44756fcb94d9d
Matheus Rodrigues Lima <mate_b10@hotmail.com>
2026-06-25 10:41:51 -0400

    feat: atribuido modelo e finalizado veiculo (Fase GREEN)

--- src/main/java/oficina/Veiculo.java
+++ src/main/java/oficina/Veiculo.java
@@ -3,17 +3,21 @@ package oficina;
 public class Veiculo {
     private String placa;
     private Cliente dono;
+    private String modelo;
 
-    public Veiculo(String placa, Cliente dono) {
+    public Veiculo(String placa, Cliente dono, String modelo) {
         this.placa = placa;
         this.dono = dono;
-    }
+        this.modelo = modelo;
 
+    }
     public String getPlaca() {
         return placa;
     }
-
     public Cliente getDono() {
         return dono;
+    }
+    public String getModelo() {
+        return modelo;
     }
 }
