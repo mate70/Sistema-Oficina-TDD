9fdcd90f3e790131ad7f0558d1ff4b8d031f05cf
Matheus Rodrigues Lima <mate_b10@hotmail.com>
2026-06-25 11:42:20 -0400

    test: teste para incrementar especialidade e matricula em mecanico (Fase RED)

--- src/test/java/oficina/MecanicoTest.java
+++ src/test/java/oficina/MecanicoTest.java
@@ -5,7 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MecanicoTest {
     @Test
     public void deveCriarMecanicoComSucesso() {
-        Mecanico mecanico = new Mecanico("João");
+        Mecanico mecanico = new Mecanico("João","Especialidade", "1234");
         assertEquals("João", mecanico.getNome());
+        assertEquals("Especialidade", mecanico.getEspecialidade());
+        assertEquals("1234", mecanico.getMatriculo());
     }
 }
