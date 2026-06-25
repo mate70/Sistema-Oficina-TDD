8ea7c79d99302205add4f0852ee248cd77f89c7f
Matheus Rodrigues Lima <mate_b10@hotmail.com>
2026-06-25 11:48:50 -0400

    feat: Implementação dos atributos especialidade e matricula em mecanico (Fase GREEN)

--- src/main/java/oficina/Mecanico.java
+++ src/main/java/oficina/Mecanico.java
@@ -2,12 +2,24 @@ package oficina;
 
 public class Mecanico {
     private String nome;
+    private String especialidade;
+    private String matriculo;
 
-    public Mecanico(String nome) {
+    public Mecanico(String nome, String especialidade, String matriculo) {
         this.nome = nome;
+        this.especialidade = especialidade;
+        this.matriculo = matriculo;
     }
 
     public String getNome() {
         return nome;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public String getMatriculo() {
+        return matriculo;
     }
 }
