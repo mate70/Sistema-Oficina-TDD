53eb09a3e6883afcbd4bbaf38a4d1a5e04d4f604
Matheus Rodrigues Lima <mate_b10@hotmail.com>
2026-06-25 13:28:47 -0400

    test: iniciando teste em servico, com nome e valor (Fase RED)

---
+++ src/test/java/oficina/ServicoTest.java
@@ -0,0 +1,14 @@
+package oficina;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ServicoTest {
+    @Test
+    public void deveCriarServicoComSucesso() {
+        // Teste para verificar a criação de um serviço
+        servico.Servico servico = new servico.Servico("alinhamento e balanceamento", 100.0);
+        assertEquals("alinhamento e balanceamento", servico.getNome());
+        assertEquals(100.0, servico.getValor());
+    }
+}
