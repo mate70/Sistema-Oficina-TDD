97f9cea125b777b492d2b1d79a1e4c501781d1ba
Matheus Rodrigues Lima <mate_b10@hotmail.com>
2026-06-25 11:29:46 -0400

    feat: Implemento da classe macanico com atributo nome (Fase GREEN)

---
+++ src/main/java/oficina/Mecanico.java
@@ -0,0 +1,13 @@
+package oficina;
+
+public class Mecanico {
+    private String nome;
+
+    public Mecanico(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
